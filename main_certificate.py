0d7e93ac27eedf35e8cf10a37ab35ccc0ed5c4af
Sabyasachi <yoboy907@gmail.com>
2023-01-17 00:18:55 +0530

    Initial Commit

---
+++ main_certificate.py
@@ -0,0 +1,66 @@
+
+import os
+
+os.system("pip install -r requirements.txt")
+
+from certificate import *
+from docx import Document
+import csv
+# from docx2pdf import convert
+
+
+# create output folder if not exist
+try:
+    os.makedirs("Output/Doc")
+    os.makedirs("Output/PDF")
+except OSError:
+    pass
+
+
+def get_participants(f):
+    data = [] # create empty list
+    with open(f, mode="r", encoding='utf-8') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            data.append(row) # append all results
+    return data
+
+def create_docx_files(filename, list_participate, ambassador):
+
+    for participate in list_participate:
+        # use original file everytime
+        doc = Document(filename)
+
+        name = participate["Name Surname"]
+        event = participate["Workshop"]
+
+        replace_participant_name(doc, name)
+        replace_event_name(doc, event)
+        replace_ambassador_name(doc, ambassador)
+        doc.save('Output/Doc/{}.docx'.format(name))
+
+        # ! if your program working slowly, comment this two line and open other 2 line.
+        print("Output/{}.pdf Creating".format(name))
+        # convert('Output/Doc/{}.docx'.format(name), 'Output/Pdf/{}.pdf'.format(name))
+
+        # ! Open those lines and comment above 2 lines if your program working extremely slow
+        # os.system("docx2pdf Output/Doc/")
+        # os.system("move Output\Doc\*pdf Output\PDF")
+
+    
+# get certificate temple path
+certificate_file = "Data/Event Certificate Template.docx"
+# get participants path
+participate_file = "Data/"+("Participant List.csv" if (input("Test Mode (Y/N): ").lower())[0]=="n" else "temp.csv")
+
+# Enter your name here [Ambassador Name]
+ambassador_name = input("Enter your name(This will be the host name on the certificates): ")
+
+# get participants
+list_participate = get_participants(participate_file);
+
+# process data
+create_docx_files(certificate_file, list_participate, ambassador_name)
+
+
+
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Microsoft Learn Student Ambassador Certificate Automation
+
+This repo simply use a template certificate docx file and generates certificates
+both docx and pdf.
+
+## Setup
+- Working on Windows only.
+
+```
+cd MLSA-Certificate-Automate
+pip install -r requirements.txt
+python main_certificate.py
+
+```
+
+## Customization
+
+In, `main_certificate.py` folder. Change your participants and your name with path and name.
+
+Also, functions are very simple. You can implement your own versions as well.
+
+## Further Releases (Planning)
+- Adding e-mail automation to script?
+- Implementing for Linux users?
+
+<h2></h2>
+
+
+Souce Repo(I made some subtle improvements) : <a href="https://github.com/muhammedogz/MLSA-Certificate-Automate">Click Here</a>
