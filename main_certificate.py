0b5a92a76f0a7ee67b9ce6dcd6bb45a22be1a343
Sabyasachi-Seal <yoboy907@gmail.com>
2023-01-25 16:59:10 +0530

    Updated Main

--- main_certificate.py
+++ main_certificate.py
@@ -3,11 +3,15 @@ import os
 
 os.system("pip install -r requirements.txt")
 
+from openpyxl import Workbook, load_workbook
+
 from certificate import *
 from docx import Document
 import csv
 from docx2pdf import convert
 
+
+mailerpath = "Data/Mail.xlsm"
 
 # create output folder if not exist
 try:
@@ -25,21 +29,55 @@ def get_participants(f):
             data.append(row) # append all results
     return data
 
+offset = 2
+
+def updatemailer(row, workbook, sheet, email, filepath, sub, body, status, cc=""):
+    sheet.cell(row=row, column=1).value = email
+    sheet.cell(row=row, column=2).value = cc
+    sheet.cell(row=row, column=3).value = sub
+    sheet.cell(row=row, column=4).value = body
+    sheet.cell(row=row, column=5).value = filepath
+    sheet.cell(row=row, column=6).value = status
+    workbook.save(filename = mailerpath)
+
+def getworkbook(filename):
+    wb = load_workbook(filename=filename, read_only=False, keep_vba=True)
+    sheet = wb.active
+    return wb, sheet
+
 def create_docx_files(filename, list_participate):
 
-    gdsc = input("Enter your college name: ")
-    lead = input("Enter GDSC Lead Name: ")
-    facilitator = input("Enter GCCP Facilitator Name: ")
-    event = input("Enter the event name: ")
+    wb, sheet = getworkbook(mailerpath)
 
-    for participate in list_participate:
+    gdsc = "s"
+    lead = gdsc
+    facilitator = gdsc
+    event = gdsc
+
+    for index, participate in enumerate(list_participate):
         # use original file everytime
         doc = Document(filename)
 
-        name = participate["Student Name"]
-
         if participate["Pathway Completion Status"] == "No":
             continue
+
+        name = participate["Student Name"]
+        email = participate["Student Email"]
+
+        sub = f"[{event}] Certificate of Completion"
+
+        body = f''' Dear {name},
+
+    Thank you for participating in {event}. 
+    Your participation is valuable to us.
+    Please find the attached certificate of completion.
+
+Thanks and Regards,
+{facilitator} 
+{event} Facilitator,
+and
+{lead}
+GDSC TMSL Lead'''
 
         replace_participant_name(doc, name)
         replace_gdsc_name(doc, gdsc)
@@ -53,9 +91,9 @@ def create_docx_files(filename, list_participate):
         print("Output/{}.pdf Creating".format(name))
         convert('Output/Doc/{}.docx'.format(name), 'Output/Pdf/{}.pdf'.format(name))
 
-        # ! Open those lines and comment above 2 lines if your program working extremely slow
-        # os.system("docx2pdf Output/Doc/")
-        # os.system("move Output\Doc\*pdf Output\PDF")
+        filepath = os.path.abspath('Output/Pdf/{}.pdf'.format(name))
+
+        updatemailer(row=(offset+index), workbook=wb,  sheet=sheet, email=email, filepath=filepath, sub=sub, body=body, status="Send")
 
     
 # get certificate temple path
