5898c5bb3ad451e503dad64446f0502f45714d51
ZAMEEL HASSAN <zekyazelu7@gmail.com>
2023-10-12 22:04:22 +0530

    email sending script added

---
+++ Data/send_mail.py
@@ -0,0 +1,76 @@
+import openpyxl
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+
+# Replace with your email configuration
+email_address = input("Enter your gmail id: ")
+email_password = input("Enter your password: ")
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+
+# Load the Excel file
+file_path = 'Mail.xlsm'
+workbook = openpyxl.load_workbook(file_path)
+worksheet = workbook.active
+
+# Find the column indices
+to_column = 'A'
+cc_column = 'B'
+subject_column = 'C'
+body_column = 'D'
+attachment_column = 'E'
+
+is_test = int(input("\nIs this for testing?\n1. Yes  2. No\n=> "))
+
+print("\nStarting to send the emails...\n")
+
+# Iterate through the rows in the Excel file
+for row in worksheet.iter_rows(min_row=2, values_only=True):  # Assuming data starts from the second row
+    if is_test == 1:
+        to_address = input("Enter to email: ")
+        cc_address = input("Enter cc emails: ")
+        subject = "[TEST] Google Cloud Certificate"
+        body = row[3]
+        attachment_path = row[4]
+    elif is_test == 2:
+        to_address = row[0]
+        cc_address = row[1]
+        subject = row[2]
+        body = row[3]
+        attachment_path = row[4]
+    else:
+        print("\nWrong input, enter either 1 or 2\n")
+        break
+    # Create an email message
+    message = MIMEMultipart()
+    message['From'] = email_address
+    message['To'] = to_address
+    message['Cc'] = cc_address
+    message['Subject'] = subject
+    message.attach(MIMEText(body, 'html'))
+
+    # Attach the file (if any)
+    if attachment_path:
+        attachment = MIMEApplication(open(attachment_path, 'rb').read())
+        attachment.add_header('Content-Disposition', 'attachment', filename='GoogleCloudCertificate.pdf')
+        message.attach(attachment)
+
+    # Connect to the SMTP server and send the email
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(email_address, email_password)
+        recipients = [to_address]
+        server.sendmail(email_address, recipients, message.as_string())
+        server.quit()
+        print(f"Email sent to {to_address}")
+    except Exception as e:
+        print(f"Failed to send email to {to_address}: {str(e)}")
+    finally:
+        if is_test == 1:
+            break
+
+# Close the Excel file
+workbook.close()
--- readme.md
+++ readme.md
@@ -2,7 +2,6 @@
 
 This repo simply use a template certificate docx file and generates certificates
 both docx and pdf.
-###  Working on Windows only.
 
 ## Run these 4 commands in your terminal
 
@@ -21,13 +20,16 @@ python main_certificate.py
 - You can change the email template file in the `Data` folder.
 
 ## How to send emails?
+### Using Macros
 - You can use the `Mail.xlms` file to send emails to the participants. Open this with Excel. Press ```Allow Content``` if required.
 - Do not need to change anything in the file itself.
 - All you need to do is to search for ```View Macros```  in excel and then select the ```Send_Mails``` macro and then click on ```Run```.
 - Now open outlook and login.
 - Click on outbox and see the mails being sent one by one.
 
-## Further Releases (Planning)
-- Implementing for Linux users?
+### Using SMTP with python
+- Run the ```send_mail.py``` script inside the Data folder
+- Enter your email and password
+- If any error raises saying that couldn't login or password is wrong, go to ```Manage your account``` in your gmail and enable ```Less secure app access```.
 
 <h2></h2>
